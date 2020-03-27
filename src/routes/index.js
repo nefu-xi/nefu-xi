beaf2d7b52b5ab06b898b14bd63c29819bf0ba45
nefu-xi <nefuxi95@gmail.com>
2020-03-27 10:58:02 -0400

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params
+  ctx.body = {
+    title: 'this is loadMore API',
+    userName,
+    pageIndex
+  }
+})
+
+module.exports = router
