148db3501fd323f825fe48292abbf2075e9e41ca
nefu-xi <nefuxi95@gmail.com>
2020-04-04 21:39:49 -0400

    feat:model

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -1,10 +1,16 @@
 /**
  * @description 数据模型入口文件
- * @author Xi
+ * @author XI
  */
 
 const User = require('./User')
+const Blog = require('./Blog')
+
+Blog.belongsTo(User, {
+    foreignKey: 'userId'
+})
 
 module.exports = {
-    User
+    User,
+    Blog
 }
